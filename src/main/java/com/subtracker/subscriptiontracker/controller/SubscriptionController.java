9bd5e7771b7c015c4eee1cc822c3b4bbf07295c4
Alex-Marin21 <alexmarinquq5@gmail.com>
2026-07-23 17:44:16 +0300

    Allow CORS from production frontend URL

--- src/main/java/com/subtracker/subscriptiontracker/controller/SubscriptionController.java
+++ src/main/java/com/subtracker/subscriptiontracker/controller/SubscriptionController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/subscriptions")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 public class SubscriptionController {
 
     @Autowired
